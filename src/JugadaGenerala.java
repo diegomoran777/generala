e72ff208d7171e66c918851277dcda17abf75a36
Diego Moran <diego.moran@comunidad.ub.edu.ar>
2019-05-18 15:57:59 -0300

    Clase JugadaPoker hecha

--- src/JugadaGenerala.java
+++ src/JugadaGenerala.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class JugadaGenerala implements Jugada {
 
+	
 	public JugadaGenerala() {
 		
 	}
--- src/Main.java
+++ src/Main.java
@@ -10,25 +10,36 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Integer> dados= new ArrayList<Integer>();
-		dados.add(1);
-		dados.add(2);
-		dados.add(3);
-		dados.add(4);
-		dados.add(6);
+		ArrayList<Integer> lista=new ArrayList<Integer>();
 		
-		ArrayList<Integer> lista= new ArrayList<Integer>();
-		lista.add(1);
-		lista.add(2);
+		lista.add(4);
+		lista.add(4);
+		lista.add(4);
 		lista.add(3);
-     	lista.add(4);
-     	
-     	
-     	for(int i=0; i<dados.size();i++) {
-     		for(int j=0;j<lista.size();j++)
-     		{
-     			
-     		}
-     	}
-	}
+		lista.add(3);
+	
+		for(int i=1;i<=6;i++)
+		{
+			int cont=0;
+			for(int j=0;j<lista.size();j++)
+			{
+				if(lista.get(j)==i)
+				{
+					cont++;
+				}
+			}
+			if(cont==4)
+			{
+				System.out.println("full");
+				i=6;
+			}
+			else {
+				if(i==6)
+				{
+					System.out.println("false");
+				}
+			}
+		}
+		
+}
 }

a2b4afba9179c0ef6ee7123e0220ddd66f811073
beelanselm <devicente.mbelen@gmail.com>
2019-06-02 22:33:54 -0300

    comentarios agregados en jugada full y poker

--- src/generala/JugadaFull.java
+++ src/generala/JugadaFull.java
@@ -34,27 +34,28 @@ public class JugadaFull implements Jugada
 	@Override
     public boolean encontrada(ArrayList<Integer> dados) 
     {   
-		//En el set se guardan los numeros sin repetirse:
-		Set<Integer> repetido = new HashSet<>();
-		for (int i = 0; i < dados.size(); i++)
-		{
-			repetido.add(i);
-            //obtengo la cantidad de veces que aparece cadavalor para filtrar
-            int freq = Collections.frequency(dados, dados.get(i));
+	//En el set se guardan los numeros sin repetirse y ordenados:
+	Set<Integer> sinRepetir = new HashSet<>();
+        //recorremos la lista con los 5 numeros que obtuvo el usuario
+            for (int i = 0; i < dados.size(); i++)
+            {//añade cada valor al set creado arriba
+                sinRepetir.add(i);
+            //obtengo la cantidad de veces que aparece cada valor para filtrar
+                int freq = Collections.frequency(dados, dados.get(i));
             //si un n aparece 4 veces ya no es jugada Full
-             if(freq == 4)
-             {
+                if(freq == 4)
+                {
             	 return false;
-             }
-        }
+                }
+            }
              /*Para que haya Full debe haber 1 n repetido 3 veces y otro n repetido 2 veces
              por lo tanto en el Set repetido solo deberia haber 2 numeros*/
-            if(repetido.size() != 2)
+            if(sinRepetir.size() != 2)
             {
             	return false;
             }
      
-            //Si hay mass de dos numeros almacenados, no hay Full, caso contrario, devuelve true
+            //Si hay mas de dos numeros almacenados, no hay Full, caso contrario, devuelve true
             return true;
     }
 }

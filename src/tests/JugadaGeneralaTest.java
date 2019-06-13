ced48ed5a4f18693e2748f52b441b6151ccda49c
Lonni <ro_0202@hotmail.com>
2019-06-13 14:24:56 -0300

    JUnit generalatest 'encontrada y nombre'

--- src/tests/JugadaGeneralaTest.java
+++ src/tests/JugadaGeneralaTest.java
@@ -1,7 +1,8 @@
 package tests;
 
-
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -10,28 +11,35 @@ import java.util.Arrays;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 
-import generala.Jugada;
 import generala.JugadaGenerala;
 
-
-
-
 public class JugadaGeneralaTest {
-
-class JugadaGeneralaTest {
 
       
 	@BeforeEach
 	
+	public void setUp() {
+		
+		j = new JugadaGenerala();
+	}
 	
 	  @Test
-	  public void testGenerala()
+	  public void testEncontrada()
 	    {
-	        Jugada j = new JugadaGenerala();
+	     
 	        assertTrue(j.encontrada((ArrayList<Integer>) Arrays.asList(1, 1, 1, 1, 1)));
 	        assertFalse(j.encontrada((ArrayList<Integer>) Arrays.asList(3, 2, 4, 5, 2)));
 	    }
-	}
+	
+	  @Test
+	  public void testNombre() {
+		  assertEquals(j.nombre(),"generala");
+		  assertNotEquals(j.nombre(), "hola");
+	  }
+
+
+ JugadaGenerala j;
+ }
 
 
 

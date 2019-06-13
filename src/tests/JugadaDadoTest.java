59e36f71cf234b80bdfaf8e87c6662d43046c0b3
Lonni <ro_0202@hotmail.com>
2019-06-13 16:31:59 -0300

    JUnit JugadaDadoTest 'encontrada'

--- src/tests/JugadaDadoTest.java
+++ src/tests/JugadaDadoTest.java
@@ -1,26 +1,28 @@
 package tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
 
-import generala.Jugada;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import generala.JugadaDado;
 
 
 public class JugadaDadoTest {
 
-class JugadaDadoTest {
+	@BeforeEach
+	public void setUp() 
+	{    d = new JugadaDado(1); 
+	}
 
-	
-	  @Test
-public void testDado()
-{
-   Jugada d = new JugadaDado(1);
+
+@Test
+public void testEncontrada() {
 
    assertTrue(d.encontrada(Arrays.asList(1, 1, 2, 3, 4)));
    assertEquals(d.puntos(), 2);
@@ -40,6 +42,8 @@ public void testDado()
 
    assertEquals(d.puntos(), 18);
 }
+
+ 	JugadaDado d;
 }
 
 

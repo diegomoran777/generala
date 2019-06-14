dfe1d0f9935ec172e825e0ba6a6e6868569f2ad1
belen <belen>
2019-06-14 14:36:20 -0300

    cambios en tests

--- src/tests/JugadaDadoTest.java
+++ src/tests/JugadaDadoTest.java
@@ -3,8 +3,6 @@ package tests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -18,9 +16,15 @@ public class JugadaDadoTest {
 	@BeforeEach
 	public void setUp() 
 	{    d = new JugadaDado(1); 
+		 d1 = new JugadaDado(6);
 	}
 
-
+@Test
+public void testNombre()
+{
+	assertEquals("1", d.nombre());
+}
+	
 @Test
 public void testEncontrada() {
 
@@ -29,21 +33,12 @@ public void testEncontrada() {
    assertFalse(d.encontrada(Arrays.asList(2, 3, 4, 5, 2)));
    assertEquals(d.puntos(), 0);
 
-   d = new JugadaDado(6);
-   assertTrue(d.encontrada(Arrays.asList(6, 6, 6)));
-
-   assertTrue(d.encontrada((ArrayList<Integer>) Arrays.asList(1, 1, 2, 3, 4)));
-   assertEquals(d.puntos(), 2);
-   assertFalse(d.encontrada((ArrayList<Integer>) Arrays.asList(2, 3, 4, 5, 2)));
-   assertEquals(d.puntos(), 0);
-
-   d = new JugadaDado(6);
-   assertTrue(d.encontrada((ArrayList<Integer>) Arrays.asList(6, 6, 6)));
-
-   assertEquals(d.puntos(), 18);
+   assertTrue(d1.encontrada(Arrays.asList(6, 6, 6)));
+   assertEquals(d1.puntos(), 18);
 }
 
  	JugadaDado d;
+ 	JugadaDado d1;
 }
 
 

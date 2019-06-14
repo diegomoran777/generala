98d66d22eda4a2f68ca342605f5b698810e29d6e
Lonni <ro_0202@hotmail.com>
2019-06-14 13:39:00 -0300

    JUnit JuegoTest 'juego'

--- src/tests/JuegoTest.java
+++ src/tests/JuegoTest.java
@@ -1,8 +1,12 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import generala.Juego;
+import generala.Jugador;
 
 class JuegoTest {
 
@@ -11,6 +15,14 @@ class JuegoTest {
 		juego = new Juego();
 	}	
 	
+	@Test
+	void juego() {
+		Jugador rocio = new Jugador("rocio");
+		Jugador belen = new Jugador("belen");
+		juego.getListaJugadores().add(belen);
+		juego.getListaJugadores().add(rocio);
+		assertEquals(juego.getListaJugadores().size(),2);			
+	}
 
  Juego juego;
 }
